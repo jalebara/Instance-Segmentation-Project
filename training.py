deea9d7862fb8acf2ba635016c1a422b02827f9c
Jeff <jabaraho@ncsu.edu>
2020-04-22 12:14:15 -0400

    model

--- training.py
+++ training.py
@@ -197,7 +197,7 @@ def display_predictions():
     fps = []
     # Pick last directory
     for d in dir_names: 
-        dir_name = os.path.join(model.model_dir, d)
+        dir_name = os.path.join(MODEL_DIR, d)
         # Find the last checkpoint
         checkpoints = next(os.walk(dir_name))[2]
         checkpoints = filter(lambda f: f.startswith("mask_rcnn"), checkpoints)
@@ -214,7 +214,7 @@ def display_predictions():
     # Recreate the model in inference mode
     model = modellib.MaskRCNN(mode='inference', 
                             config=config,
-                            model_dir=ROOT_DIR)
+                            model_dir=MODEL_DIR)
 
     # Load trained weights (fill in path to trained weights here)
     assert model_path != "", "Provide path to trained weights"
