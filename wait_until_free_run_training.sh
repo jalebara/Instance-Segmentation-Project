ee670fa27ea106127de2a66d07da45f06df81cf0
Jeff <jabaraho@ncsu.edu>
2020-04-22 20:25:02 -0400

    revise

--- wait_until_free_run_training.sh
+++ wait_until_free_run_training.sh
@@ -4,22 +4,26 @@ ProjectDir=/home/jabaraho/coding/ECE542FinalProject
 
 cd $ProjectDir
 
+conda init
+
 for i in {1..12}
 do
-    if [ pgrep -x python >> /dev/null ]
+    if [ "$(pgrep -x python >> /dev/null)" ]
     then
         echo "Computer Still in Use"
-        sleep(3600)
+        sleep 3600
     else
         echo "Ready for 5 epoch run"
         conda activate ece542projects
 
-        if [ "$(ls -A /path/to/directory)" ]
+        if [ "$(ls -A $ProjectDir/logs)" ]
         then
             python ./training.py --train_from_checkpoint
         else
             python ./training.py --train_model
         fi
+
+        deactivate
     fi
 done
 
