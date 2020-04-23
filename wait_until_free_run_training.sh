55e638ab7533cf40db3a9d7bb15bb9db9e6cbb63
Jeff <jabaraho@ncsu.edu>
2020-04-22 21:22:24 -0400

    automated training

--- wait_until_free_run_training.sh
+++ wait_until_free_run_training.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 ProjectDir=/home/jabaraho/coding/ECE542FinalProject
+$HomeDir=/home/jabaraho
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 
 cd $ProjectDir
 
-
-for i in {1..12}
+for i in {1..8}
 do
     if pgrep -x python >/dev/null
     then
@@ -14,12 +20,31 @@ do
     else
         echo "Ready for 5 epoch run"
 
+        if [ "$(ls -A $ProjectDir/data)" ]
+        then 
+            echo "Data Already Loaded"
+        else
+            echo "Generate Ramdisk for data"
+            mount -t tmpfs -o size=13G tmpfs $ProjectDir/data
+            
+            unzip $HomeDir/Downloads/leftImg8bit_trainvaltest.zip -d $ProjectDir/data || exit 1
+            mv -f $ProjectDir/data/leftImg8bit_trainvaltest/* $ProjectDir/data || exit 1
+            rm -rf $ProjectDir/data/leftImg8bit_trainvaltest || exit 1
+
+            unzip $HomeDir/Downloads/gtFine_trainvaltest.zip -d $ProjectDir/data || exit 1
+            rsync --remove-source-file -a $ProjectDir/data/gtFine || exit 1
+            rm -rf $ProjectDir/data/gtFine || exit 1
+
         if [ "$(ls -A $ProjectDir/logs)" ]
         then
-            python ./training.py --train_from_checkpoint
+            python ./training.py --train_from_checkpoint || { echo "Checkpoint Training Failed... Releasing Ramfs" ; umount $ProjectDir/data ; exit 1 ; }
         else
-            python ./training.py --train_model
+            python ./training.py --train_model || { echo "Initial Training Failed... Releasing Ramfs" ; umount $ProjectDir/data ;  exit 1 ; }
         fi
     fi
 done
 
+if [ "$(ls -A $ProjectDir/data)" ]
+then 
+    echo "Releasing Ramfs"
+    umount $ProjectDir/data
